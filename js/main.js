1df762f0dd102c837f38ad2d92b6ac64a44590d6
blackblue2712 <lars27399@gmail.com>
2020-03-22 20:42:25 +0700

    resume

---
+++ js/main.js
@@ -0,0 +1,21 @@
+window.onload = () => {
+    document.querySelector(".hd-right").addEventListener("mouseover", function() {
+        document.querySelector(".header-content").style.opacity = "0";
+    })
+
+    document.querySelector(".hd-right").addEventListener("mouseleave", function() {
+        document.querySelector(".header-content").style.opacity = "1";
+    })
+}
+
+window.onscroll = function(e) {
+    let scrollY = window.scrollY;
+    if(scrollY > 20) {
+        document.querySelector("#scroll-up").style.display = "block";
+        document.querySelector(".self-picture").style.display = "block";
+    } else {
+        document.querySelector("#scroll-up").style.display = "none";
+        document.querySelector(".self-picture").style.display = "none";
+    }
+    document.querySelector(".background-parallax").style.backgroundPositionY = - (scrollY/2) + "px";
+}
